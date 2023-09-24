92aced062b5e150b49e2b224cc4e2ea270621a19
Snowy <snowy@snowymouse.com>
2023-09-24 12:55:37 +0200

    Commit missing mods dir

---
+++ mods/mod_enables.asm
@@ -0,0 +1,5 @@
+ModPokerusSettings:: db MOD_RNG_POKERUS_NORMAL
+ModSoloModSettings:: db $FF
+ModRNGSettings::     db $FF
+ModMiscSettings::    db $FF
+ModFixedIVs::        dw $FFFF
---
+++ mods/mod_features.asm
@@ -0,0 +1,21 @@
+; Emulate a solo run for battles
+DEF MOD_SOLO_MOD_VISUAL                 EQU 0 ; show 1 ball when entering battle
+DEF MOD_SOLO_MOD_BLACKOUT_AUTOMATICALLY EQU 1 ; blackout if your leader faints
+DEF MOD_SOLO_MOD_AI_MODIFICATIONS       EQU 2 ; AI ignore other party members for choosing moves
+DEF MOD_SOLO_MOD_BEAT_UP_MODIFICATIONS  EQU 3 ; beat up only uses slot 1
+DEF MOD_SOLO_MOD_PHASING_MODIFICATIONS  EQU 4 ; block roar/whirlwind
+
+; Change stat EXP
+DEF MOD_RNG_EARLY_ENCOUNTERS_DISABLED   EQU 0 ; disable early encounters in requires routes until Kurt is spoken to
+DEF MOD_RNG_SPINNERS_DISABLED           EQU 1 ; random spinners will ignore you
+DEF MOD_RNG_GUARANTEE_RODS              EQU 2 ; rods always work
+DEF MOD_RNG_GUARANTEE_CATCHES           EQU 3 ; balls always work
+DEF MOD_RNG_GUARANTEE_STARTER_IVS       EQU 4 ; starter IVs are fixed
+
+; Misc. changed
+DEF MOD_MISC_LEVEL_EVOLUTIONS_DISABLED  EQU 0 ; evolution is disabled
+
+; Pokerus settings
+DEF MOD_RNG_POKERUS_NORMAL              EQU 0 ; normal behavior
+DEF MOD_RNG_POKERUS_DISABLED            EQU 1 ; disabled
+DEF MOD_RNG_POKERUS_GUARANTEED          EQU 2 ; guaranteed pokerus (requires visiting goldenrod still)
